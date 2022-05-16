ed89ec3522e93f4a1d2c1e6c8c5539017c110b12
Jeff Bucklew <bucklew.jeff@gmail.com>
2022-05-15 20:29:51 -0400

    saving leftover work to a new branch

--- oracle.py
+++ oracle.py
@@ -1,19 +1,20 @@
+import subprocess
+
 import serial
 import time
 import os
 import random
 import subprocess
 
+
 # connect to Arduino
 serial_path = '/dev/ttyUSB0'
 serial_input = serial.Serial(serial_path, 9600)  # this auto-opens the port
 time.sleep(2)  # wait for Arduino, which resets when serial conn opened
 
-# default .mov
-sleep_mov_path = '../the_oracle_mov/sleep.mov'
-
 # answer .movs
 answer_mov_root = '../the_oracle_mov/answers/'
+# look up os.expandpath
 answer_movs = os.listdir(answer_mov_root)
 
 # playing .mov files from python is ... difficult
@@ -21,15 +22,25 @@ answer_movs = os.listdir(answer_mov_root)
 # https://wiki.videolan.org/VLC_command-line_help/
 
 # DRY & comply with PEP8
-# --no-qt-video-autoresize
-vlc_bash = 'cvlc --fullscreen --no-autoscale --no-qt-video-autoresize --no-video-title-show --no-interact '
-play_sleep_bash = vlc_bash + '--repeat ../the_oracle_mov/sleep.mov'
+# --no-qt-video-autoresize --fullscreen --no-autoscale --one-instance
+# vlc_bash = ' cvlc --no-video-title-show --no-interact --one-instance --fullscreen '
+# mov_process = subprocess.Popen(vlc_bash.split(), stdin=subprocess.PIPE)
+mov_process = subprocess.run('/bin/bash', shell=True, universal_newlines=True, stdin=subprocess.PIPE)
 
+vlc_bash = ' vlc --no-video-title-show --no-interact --one-instance --fullscreen'
+# play_sleep_bash = vlc_bash + b'--repeat ../the_oracle_mov/Sleeping.mp4'
+play_sleep_bash = vlc_bash + ' --repeat ../the_oracle_mov/Sleeping.mp4\n'
 # start default sleep mov, non-blocking so interference can be caught
-sleep_process = subprocess.Popen(play_sleep_bash.split())
+# sleep_process = subprocess.Popen(play_sleep_bash.split())
+# mov_process.stdin.write(vlc_bash + play_sleep_bash)
+mov_process.communicate(play_sleep_bash)
+#mov_process.stdin.write(play_sleep_bash)
+#mov_process.stdin.close()
+#mov_process.stdout.read()
 
 while True:
     # since we're jumping in mid-stream, the try/except will make sure we wait for good data
+    mov_process.communicate(play_sleep_bash)
     try:
         # https://pyserial.readthedocs.io/en/latest/shortintro.html#readline
         value = serial_input.readline().strip().decode("utf-8")  # format for easy digestion - '3XX'
@@ -40,26 +51,34 @@ while True:
         if len(value) is 3 and int(value) < 300:  # interference
 
             # terminate sleep process
-            sleep_process.terminate()  # non-blocking process only requires terminate() to stop
+            # sleep_process.terminate()  # non-blocking process only requires terminate() to stop
+            # mov_process.terminate()
 
             # get a random idx for selecting random answer .mov
-            answer_index = random.randint(0, len(answer_movs) - 1)
+            # answer_index = random.randint(0, len(answer_movs) - 1)
 
             # may need  --one-instance --play-and-exit; doesn't need --playlist-enqueue as that's default behavior
-            play_answer_bash = vlc_bash + '--play-and-exit ' + answer_mov_root + answer_movs[answer_index]
+            # play_answer_bash = vlc_bash + '--play-and-exit ' + answer_mov_root + answer_movs[answer_index]
 
             # alternate: use vlc random arg an pass the whole answer dir
-            # play_answer_bash = vlc_bash + '--play-and-exit --random --fullscreen ' + answer_mov_root
+            play_answer_bash = vlc_bash + '--play-and-exit --random ' + answer_mov_root
 
             # queue the answer .mov
-            answer_process = subprocess.Popen(play_answer_bash.split())
+            # answer_process = subprocess.Popen(play_answer_bash.split())
             # answer_process = subprocess.Popen(play_answer_bash.split(), stdout=subprocess.PIPE)
+            # mov_process.stdin.write(play_answer_bash)
+            # mov_process.stdin.flush()
+            mov_process.communicate(play_answer_bash)
 
             # calling wait() on the object returned from Popen will block until it completes.
-            answer_process.wait()
+            # answer_process.wait()
+            # mov_process.wait()
 
             # fire up sleep mov when answer vid finishes
-            sleep_process = subprocess.Popen(play_sleep_bash.split())
+            # sleep_process = subprocess.Popen(play_sleep_bash.split())
+            # mov_process.stdin.write(play_answer_bash)
+            # mov_process.stdin.flush()
+            mov_process.communicate(play_sleep_bash)
 
             # flush buffer
             serial_input.reset_input_buffer()
---
+++ print_serial_input.ino/print_serial_input.ino.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+  
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // print out the value you read:
+  Serial.println(sensorValue);
+  Serial.write(sensorValue);
+  delay(1);        // delay in between reads for stability
+}
