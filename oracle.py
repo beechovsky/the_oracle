0987ffdbe0f88f395ad1d310be8cfc4a504c6830
Jeff Bucklew <bucklew.jeff@gmail.com>
2023-02-28 20:32:58 -0500

    use list player to facilitate cleaner swapping

--- oracle.py
+++ oracle.py
@@ -4,12 +4,11 @@ import os
 import random
 import vlc
 import keyboard # requires root/sudo
-# import subprocess # may be handy for blocking during answer playback, or looping sleep mov wiuthout two instances below
 
 # playing .mov files from python is ... difficult
 # using python vlc bindings
 # https://www.olivieraubert.net/vlc/python-ctypes/doc/
-# https://wiki.videolan.org/VLC_command-line_help/
+# https://wiki.videolan.org/VLC_command-line_help/ (this is pretty much useless)
 
 ################
 # ARDUINO BITS #
@@ -30,39 +29,37 @@ sleep_mov_path = '../the_oracle_mov/sleep.mov'
 answer_mov_root = '../the_oracle_mov/answers/'
 answer_movs = os.listdir(answer_mov_root)
 
-# Using '--input-repeat=999999' made my soul hurt but this API is rubbish and this is the only thing that works.
+# Using '--input-repeat=999999' made my soul hurt, but this API is rubbish and this is the only method that actually works.
 vlc_instance = vlc.Instance('--input-repeat=999999')
 
-# TWO OPTIONS: MediaPlayer, and MediaListPlayer. The former has more built-in methods.
-player = vlc_instance.media_player_new()
-#list_player = vlc_instance.media_list_player_new()
-#media_list = vlc_instance.media_list_new()
-player.set_fullscreen(True)
+# TWO OPTIONS: MediaPlayer, and MediaListPlayer. The former has more built-in methods, but the latter allows more graceful video swapping.
+list_player = vlc_instance.media_list_player_new()
+media_list = vlc_instance.media_list_new()
 
 sleep_media = vlc_instance.media_new(sleep_mov_path) # or media_new_path? there are, of course, multiple ways to do this
-# media_list.add_media(sleep_media)
-# list_player.set_media_list(media_list)
-# list_player.play()
+media_list.add_media(sleep_media)
+list_player.set_media_list(media_list)
+
+list_player.get_media_player().set_fullscreen(True)
 
 def play_sleep_mov():
-    player.set_media(sleep_media) # forces starting at beginning, or should; otherwise it doesn't need to be in here
-    player.play()
-    # media_list.add_media(sleep_media)
-    # list_player.set_media_list(media_list)
-    # list_player.play()
+    list_player.play_item_at_index(0)
 
-print('Starting sleep .mov ...')
-play_sleep_mov()
 
 ###########
 # CONTROL #
 ###########
-keyboard.add_hotkey("Esc", lambda: player.set_fullscreen(False))
+keyboard.add_hotkey("Esc", lambda: list_player.get_media_player().set_fullscreen(False))
 
+#print('Starting sleep .mov ...')
+play_sleep_mov()
+
+########
+# LOOP #
+########
 while True:
-    # TODO: need a way to exit gracefully when in fullscreen
     
-    # since we're jumping in mid-stream, the try/except will make sure we wait for good data
+    # since we're jumping in mid-stream, the try/except will make sure we wait for good data (hacky, but there were issues reprogramming the arduino)
     try:
         # https://pyserial.readthedocs.io/en/latest/shortintro.html#readline
         value = serial_input.readline().strip().decode("utf-8")  # format for easy digestion - '3XX'
@@ -73,29 +70,28 @@ while True:
             answer_index = random.randint(0, len(answer_movs) - 1)
             answer_path = answer_mov_root + answer_movs[answer_index]
             answer_media = vlc_instance.media_new_path(answer_path)
-            print('Playing answer .mov ...')
-            player.set_media(answer_media) # adding to a playlist would be smoother
-            player.play()
-            #media_list.remove_index(0) # trim the list; remove sleep .mov
-            #media_list.add_media(answer_media)
-            #list_player.set_media_list(media_list)
-            #print('playing answer .mov ...') 
-            #list_player.play() # next() if we leave the sleep movie in
+            media_list.add_media(answer_media) # gracefully add answer media; don't clobber sleep media.
+            # print("MEDIA LIST SIZE: " + str(media_list.count()))
+            list_player.set_media_list(media_list) # unsure re-adding is necessary
+            # print('playing answer .mov ...') 
+            list_player.next() # next() if we leave the sleep movie in
     
             # block interference during answer playback
-            time.sleep(1) # dunno if this is necessary
-            duration = player.get_length() / 1000
+            time.sleep(1)
+            duration = list_player.get_media_player().get_length() / 1000
+            # print('DURATION: ' + str(duration))
             time.sleep(duration - 1)
             
-            # transition is a little rough. try two instances of vlc?
-            print('Replaying answer .mov ...')
+            # print('Replaying sleep .mov ...')
             play_sleep_mov()
+            media_list.remove_index(1) # gracefully remove answer .mov to avoid clobbering the sleep .mov and keep the playlist from growing
+            # print("MEDIA LIST SIZE: " + str(media_list.count()))
             
             # flush buffer
             serial_input.reset_input_buffer()
 
     # ignore errors caused by grabbing values mid-byte and try again until we get the beginning
-    # TODO: If we were simply getting/sending start/stop signals between the components, this hack wouldn;t be necessary.
+    # TODO: If we were simply getting/sending start/stop signals between the components, this hack wouldn't be necessary.
     except UnicodeDecodeError:
         pass
     except ValueError:
